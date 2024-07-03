b83c706183ab743dfc1c8dfd6fd2a770de74c1f4
useruserriely <psy1004.syp@gmail.com>
2024-07-03 13:24:26 +0900

    social login solve

--- src/main/java/personal/project/TripPin/MainController.java
+++ src/main/java/personal/project/TripPin/MainController.java
@@ -10,8 +10,7 @@ import java.util.List;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String home(Model model) {
-        return "index";
+    public String home() {
+        return "index"; // 메인 페이지 뷰 파일 (index.html)
     }
-
 }
