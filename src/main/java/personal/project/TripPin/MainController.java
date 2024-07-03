2492aebf02327c9b5e85bb778c10dad579547f21
useruserriely <psy1004.syp@gmail.com>
2024-07-03 17:40:54 +0900

    image url solve

--- src/main/java/personal/project/TripPin/MainController.java
+++ src/main/java/personal/project/TripPin/MainController.java
@@ -1,16 +1,25 @@
 package personal.project.TripPin;
 
 import jakarta.persistence.GeneratedValue;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import personal.project.TripPin.image.Image;
+import personal.project.TripPin.image.ImageService;
 
 import java.security.Principal;
 import java.util.List;
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final ImageService imageService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Image> images = imageService.getDefaultImages();
+        model.addAttribute("images", images);
+
         return "index"; // 메인 페이지 뷰 파일 (index.html)
     }
 }
---
+++ src/main/java/personal/project/TripPin/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package personal.project.TripPin.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
